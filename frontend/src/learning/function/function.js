d309e37b3756b7d5b3070e1eab6e3e10219f0172
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-09-14 14:51:16 +0700

    Membuat Latihan Function

---
+++ frontend/src/learning/function/function.js
@@ -0,0 +1,12 @@
+export default function sapa(nama) {
+  console.log(`Hallo ${nama}`);
+}
+
+sapa('Ramdani');
+
+function tambah(a, b) {
+  return a + b;
+}
+
+const hasil = tambah(10, 20);
+console.log(hasil);
