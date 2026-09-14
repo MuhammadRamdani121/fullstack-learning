be77a1005a8e9542d063f14696964bfc1a85b871
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-09-14 10:37:36 +0700

    Belajar: Latihan JavaScript Objects

---
+++ frontend/src/learning/object.js
@@ -0,0 +1,19 @@
+const product = {
+  user: 'Admin',
+  nama: 'Ramdani',
+  umur: 17,
+  mahasiswa: true,
+};
+
+// Panggil Menggunakan dot Notation
+console.log(product.nama);
+console.log(product.mahasiswa);
+console.log(product.umur);
+
+// Ubah Object
+product.umur = 20;
+console.log(product.umur);
+
+// Tambah Object
+product.tinggal = 'Banten';
+console.log(product.tinggal);
